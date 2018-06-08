c59721f90d2a1a6ca6e1c6f4b1ce3af104a949b3
lluis Andino <luisandinojr1@brandeis.edu>
2018-06-07 21:35:14 -0400

    desent product
    dessent product

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var calendarRouter = require('./routes/calendar');
+var profileRouter = require('./routes/profile');
+var messageRouter = require('./routes/message');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/calendar', calendarRouter);
+app.use('/profile', profileRouter);
+app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Profile.js
+++ routes/Profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'uniworkery' });
+  res.render('profile', { title: 'form demo' });
 });
 
 module.exports = router;
